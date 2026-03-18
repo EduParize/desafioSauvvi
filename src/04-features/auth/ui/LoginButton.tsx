f8ad8ac2f5f4aaa4b1e64af21e4837924b4c0a4d
EduParize <eduardoparize.05@gmail.com>
2026-03-18 16:33:06 -0300

    refactor: clean up commented code in LoginButton component

--- src/04-features/auth/ui/LoginButton.tsx
+++ src/04-features/auth/ui/LoginButton.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Alert } from 'react-native';
+//import { HeaderWidget } from '@/03-widgets/header';
 
 // MAGIC 1: The Alias (@/) and FSD rules at work!
 import { Button } from '@/06-shared/ui/Button';
 
 export const LoginButton = () => {
+  //HeaderWidget(); //Showing that we cant use widgets in features, but we can use shared components
   const handleLogin = () => {
     Alert.alert('Success', 'Login performed by the Feature!');
   };
